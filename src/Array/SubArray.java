67e59b2319a3c31d27b390bedb0bae3f9d986cec
Hemant Baviskar <hemantyb22@gmail.com>
2025-10-14 23:09:03 +0530

    till subarray

--- src/Array/SubArray.java
+++ src/Array/SubArray.java
@@ -1,4 +1,5 @@
-// 18 September
+// 10 September
+// 18 
 // 19
 
 package Array;
@@ -58,22 +59,46 @@ public class SubArray {
 		}
 	}
 
+	public static void printAllSubArray2(int[] arr) {
+		for (int size = 1; size <= arr.length; size++) {
+			for (int i = 0; i <= arr.length - size; i++) {
+				for (int j = i; j < i + size; j++) {
+					System.out.print(arr[j] + " ");
+				}
+				System.out.println();
+			}
+		}
+	}
+
 	public static void printAllSubArraySum(int[] arr) {
 		for (int i = 1; i <= arr.length; i++) {
 			printSubArraySum(arr, i);
 		}
 	}
 
+	public static void printAllSubArraySum2(int[] arr) {
+		for (int size = 1; size <= arr.length; size++) {
+			for (int i = 0; i <= arr.length - size; i++) {
+				int sum = 0;
+				for (int j = i; j < i + size; j++) {
+					sum += arr[j];
+				}
+				System.out.println(sum);
+			}
+		}
+	}
+
 	public static int countAllSubArraySum(int[] arr, int k) {
 		int count = 0;
 		for (int size = 1; size <= arr.length; size++) {
-			for (int i = 0; i < arr.length - 1; i++) {
+			for (int i = 0; i <= arr.length - size; i++) {
 				int sum = 0;
-				for (int j = i; j < arr.length; j++) {
+				for (int j = i; j < i + size; j++) {
 					sum += arr[j];
 				}
-				if (sum == k)
+				if (sum == k) {
 					count++;
+				}
 			}
 		}
 		return count;
@@ -91,6 +116,25 @@ public class SubArray {
 						System.out.print(arr[j] + " ");
 					}
 					System.out.println();
+					return;
+				}
+			}
+		}
+	}
+
+	public static void printAllSubArraySumDesc(int[] arr, int k) {
+		for (int size = arr.length - 1; size >= 0; size--) {
+			for (int i = 0; i <= arr.length - size; i++) {
+				int sum = 0;
+				for (int j = i; j < i + size; j++) {
+					sum += arr[j];
+				}
+				if (sum == k) {
+					for (int j = i; j < i + size; j++) {
+						System.out.print(arr[j] + " ");
+					}
+					System.out.println();
+					return; // if want to return largest
 				}
 			}
 		}
--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
+			int size = sc.nextInt();
 			int a = sc.nextInt();
 			int[] arr1 = new int[a];
 			for (int i = 0; i < arr1.length; i++) {
 				arr1[i] = sc.nextInt();
 			}
-			int b = sc.nextInt();
-			int[] arr2 = new int[b];
-			for (int i = 0; i < arr2.length; i++) {
-				arr2[i] = sc.nextInt();
-			}
-			int c = sc.nextInt();
-			int[] arr3 = new int[c];
-			for (int i = 0; i < arr3.length; i++) {
-				arr3[i] = sc.nextInt();
-			}
-			int[] res = Array.Merge.mergeThreeArray(arr1, arr2, arr3);
-			for (int i = 0; i < res.length; i++) {
-				System.out.print(res[i] + " ");
-			}
+			Array.SubArray.printAllSubArraySum(arr1, size);
 		}
 	}
 }
