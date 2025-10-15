f59ce8e4f34a6addef484919413ea9bdcea968e7
Hemant Baviskar <hemantyb22@gmail.com>
2025-10-15 15:19:12 +0530

    started string problems

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
-			int a = sc.nextInt();
-			int[] arr1 = new int[a];
-			for (int i = 0; i < arr1.length; i++) {
-				arr1[i] = sc.nextInt();
-			}
-			Array.Rearrange.rearrangeArray2(arr1);
-			for (int i = 0; i < arr1.length; i++) {
-				System.out.print(arr1[i] + " ");
-			}
+			String s = sc.nextLine();
+			String res = Strings.Traverse.reverseString(s);
+			System.out.println(s.equals(res) ? "Palindrome" : "Not");
 		}
 	}
 }
---
+++ src/Strings/Traverse.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Traverse {
+	public static String reverseString(String s) {
+		String res = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			res += s.charAt(i);
+		}
+		return res;
+	}
+}
