3ec1fc88b28b5d9c3550421ae24144ed85ee91fd
Hemant Baviskar <hemantyb22@gmail.com>
2025-10-19 01:30:21 +0530

    added sort class

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
 			String s1 = sc.nextLine();
-			String[] res = Strings.Word.printEachWord2(s1);
-			for (int i = 0; i < res.length; i++) {
-				System.out.print(res[i] + " ");
-			}
-			System.out.println();
-
-			for (int i = res.length - 1; i >= 0; i--) {
-				System.out.print(res[i] + " ");
-			}
-			System.out.println();
-			
-			for (int i = 0; i < res.length; i++) {
-				System.out.print(res[i] + res[i].length() + " ");
-			}
-			System.out.println();
-			
-			for (int i = 0; i < res.length; i++) {
-				System.out.print(Strings.Traverse.reverseString(res[i]) + " ");
-			}
-			System.out.println();
-			
-			for (int i = res.length - 1; i >= 0; i--) {
-				System.out.print(Strings.Traverse.reverseString(res[i]) + " ");
-			}
-			System.out.println();
-			
-			for (int i = 1; i < res.length; i+= 2) {
-				System.out.print(res[i] + res[i - 1] + " ");
-			}
-			System.out.println();
-			
-			int start = 0;
-			int end = res.length - 1;
-			while (start < end) {
-				System.out.print(res[start] + res[end] + " ");
-				start++;
-				end--;
-			}
-			System.out.println();
-			
+			System.out.println(Strings.Sort.sortString(s1));
 		}
 	}
 }
--- src/Strings/Sort.java
+++ src/Strings/Sort.java
@@ -2,15 +2,17 @@ package Strings;
 
 public class Sort {
 	public static String sortString(String s) {
-		String res = "";
-		for (int i = 0; i < s.length(); i++) {
-			for (int j = i; j < s.length(); j++) {
-				if (s.charAt(i) > s.charAt(j)) {
-					res += s.charAt(i);
+		char[] chars = s.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			for (int j = i + 1; j < chars.length; j++) {
+				if (chars[i] > chars[j]) {
+					char temp = chars[i];
+					chars[i] = chars[j];
+					chars[j] = temp;
 				}
 			}
 		}
-		return res;
+		return new String(chars);
 	}
 
 }
